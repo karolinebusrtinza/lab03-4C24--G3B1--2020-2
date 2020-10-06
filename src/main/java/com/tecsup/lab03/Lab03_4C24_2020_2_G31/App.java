9f349e7bb816c29933a050c9352e988398ead9cf
karoline roxana <bustinzakaroline@gmail.com>
2020-10-06 16:31:58 -0500

    first commit

---
+++ src/main/java/com/tecsup/lab03/Lab03_4C24_2020_2_G31/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.lab03.Lab03_4C24_2020_2_G31;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
